ed6a6ccc4922ef4c0c29e54793679889ed34bf57
El Mustapha Zahiri <ezahiri@e3r3p4.1337.ma>
2024-03-03 20:33:43 +0100

    01

--- MANDATORY/srcs/main.c
+++ MANDATORY/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: ezahiri <ezahiri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/03 01:40:08 by ezahiri           #+#    #+#             */
-/*   Updated: 2024/03/03 20:32:11 by ezahiri          ###   ########.fr       */
+/*   Updated: 2024/03/03 20:32:55 by ezahiri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@ char	**fill_strs(char **av, int ac)
 	strs = ft_split(s, ' ');
 	free(s);
 	if (is_duplicate(strs))
-		ft_exit(memfree(s));
+		ft_exit(memfree(strs));
 	return (strs);
 }
 
